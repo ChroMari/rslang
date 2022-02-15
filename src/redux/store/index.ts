accc89a69c517518652cb0f3bb68ad6f92dcfabb
skyress9 <lion.super2@gmail.com>
2022-02-15 18:37:12 +0300

    feat: add userSelectors

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,8 +1,16 @@
 import {configureStore} from "@reduxjs/toolkit";
 import userReducer from '../slices/userSlice';
+import bookReducer from "../slices/bookSlice";
+import wordsReducer from "../slices/wordsSlice";
 
-export default configureStore({
+const store = configureStore({
   reducer: {
+    book: bookReducer,
+    words: wordsReducer,
     user: userReducer,
   }
 });
+
+export default store;
+
+export type StoreType = ReturnType<typeof store.getState>;
