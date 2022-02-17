5751d8ad4258fad42185572736e479654a52c9ff
Ilya Chigrinov <chig-ilya2010@yandex.ru>
2022-02-17 19:03:41 +0300

    feat: fix sprint game

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import { ModalUser } from './components/modalUser/ModalUser';
 import { Layout } from './pages/Layout';
 import { Routes, Route } from 'react-router-dom';
 import { Header } from './components/header/Header';
+import { DifficultyLevel } from './sprint/DifficultyLevel';
+import { useSelector } from 'react-redux';
+import { StoreType } from './redux/store';
+import { SprintResults } from './sprint/SprintResults';
+import { Sprint } from './sprint/Sprint';
 
 function App() {
+    const currentTime = useSelector((state:StoreType) => state.toggleTime.currentTime)
+    const wordsArray = useSelector((state:StoreType) => state.addSprintWords.sprintWords)
     return (
         <div className="App">
             <ModalUser />
@@ -14,6 +21,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Layout />} />
                 <Route path="/book" element={<Book />} />
+                <Route path="/sprint" element={currentTime === 0 ? <SprintResults/> : (wordsArray.length !== 30 ? <DifficultyLevel /> : <Sprint />)} />
             </Routes>
         </div>
     );
